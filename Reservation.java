647aa3fcb0a6b96f8f9d4d32d0586ae6a4df025b
Saud1101 <sau3o0di@gmail.com>
2026-04-02 19:46:48 +0300

    Add files via upload

---
+++ Reservation.java
@@ -0,0 +1,40 @@
+
+public class Reservation {
+
+	private String checkInDate;
+    private String checkOutDate;
+    private Room room;
+
+    public Reservation(Room room) {
+        this.room = room;
+        this.checkInDate = "";
+        this.checkOutDate = "";
+        room.setAvailable(false); //room is unavailable by default
+    }
+
+    public void setCheckInDate(String date) {
+        this.checkInDate = date;
+    }
+
+    public void setCheckOutDate(String date) {
+        this.checkOutDate = date;
+    }
+   
+    public String getCheckInDate() { 
+    	return checkInDate; 
+    }
+   
+    public String getCheckOutDate() {
+    	return checkOutDate; 
+    	}
+    
+    public Room getRoom() {
+    	return room; 
+    	}
+
+    public String toString() {
+        return "Room: " + room.getRoomNumber() +
+               " | Check-in: " + checkInDate +
+               " | Check-out: " + checkOutDate;
+    }
+}
